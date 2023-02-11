f535f11966e78428e363ff4347bd5df8041afb68
IgorCrestian <123649172+IgorCrestian@users.noreply.github.com>
2023-02-11 23:24:19 +0100

    Homework1Java

---
+++ HomeworkJava1.java
@@ -0,0 +1,146 @@
+public class HomeworkJava1 {
+    public static void main(String[] args) {
+//        int a = 333;
+//        System.out.println(a);
+//        System.out.println();
+//
+//         //16->10
+//        System.out.println(333/16);     //20
+//        System.out.println(333%16);         //13
+//        System.out.println();
+//
+//        System.out.println(20/16);     //1
+//        System.out.println(20%16);         //4
+//        System.out.println();
+//
+//        System.out.println(1/16);     //0
+//        System.out.println(1%16);         //1
+//        System.out.println();
+//
+//        int a16 = 0x14d;
+//        System.out.println("Result 333 (16->10) =" + a16);
+
+
+        //1.   200345
+
+//        int b = 200345;
+//
+//        System.out.println(2 * Math.pow(10,5 ) + 0 * Math.pow(10,4 ) + 0 * Math.pow(10,3) + 3 * Math.pow(10,2) +
+//                4 * Math.pow(10,1) + 5 * Math.pow(10,0));
+//        System.out.println();
+
+        //2.  637 = 10->16, 16->10
+
+//        int a = 637;
+//
+//        // 10->16
+//        System.out.println("Result (10->16)  =  "+ Integer.toHexString(a));
+//
+//        //16->10
+//        System.out.println(637/16);     //39
+//        System.out.println(637%16);         //13
+//        System.out.println();
+//
+//        System.out.println(39/16);     //2
+//        System.out.println(39%16);          //7
+//        System.out.println();
+//
+//        System.out.println(2/16);     //0
+//        System.out.println(2%16);           //2
+//        System.out.println();
+//
+//          int b16 =0x27d;
+
+        //3.  637 = 10->2
+//         int a = 637;
+//
+//        System.out.println(637 / 2);  // 318
+//        System.out.println(637 % 2);            //1
+//        System.out.println();
+//
+//        System.out.println(318 / 2);   //159
+//        System.out.println(318% 2);             //0
+//        System.out.println();
+//
+//        System.out.println(159/ 2);   //79
+//        System.out.println(159% 2);             //1
+//        System.out.println();
+//
+//        System.out.println(79 / 2);   //39
+//        System.out.println(79 % 2);             //1
+//        System.out.println();
+//
+//        System.out.println(39/2);      //19
+//        System.out.println(39%2);                //1
+//        System.out.println();
+//
+//        System.out.println(19/2);      //9
+//        System.out.println(19%2);                //1
+//        System.out.println();
+//
+//        System.out.println(9/2);      //4
+//        System.out.println(9%2);                //1
+//        System.out.println();
+//
+//        System.out.println(4/2);      //2
+//        System.out.println(4%2);                //0
+//        System.out.println();
+//
+//        System.out.println(2/2);      //1
+//        System.out.println(2%2);                //0
+//        System.out.println();
+//
+//        System.out.println(1/2);      //0
+//        System.out.println(1%2);                //1
+//        System.out.println();
+//
+//
+//      int a2=0b1001111101;
+//
+//      System.out.println("Result (10->2) =" + Integer.toBinaryString(a));
+
+        //4.   11100111 -> 10
+
+//        int a10 = 11100111;
+//        System.out.println(1 * Math.pow(2,7) + 1 * Math.pow(2,6) + 1 * Math.pow(2,5) + 0 * Math.pow(2,4) +0
+//        * Math.pow(2,3) + 1 * Math.pow(2,2) + 1 * Math.pow(2,1) + 1 * Math.pow(2,0));
+
+
+        //5.    637 = 10->3
+//        int a = 637;
+//
+//
+//        System.out.println(637 / 3);  // 212
+//        System.out.println(637 % 3);            //1
+//        System.out.println();
+//
+//        System.out.println(212 / 3);   //70
+//        System.out.println(212 % 3);             //2
+//        System.out.println();
+//
+//        System.out.println(70 / 3);   //23
+//        System.out.println(70 % 3);             //1
+//        System.out.println();
+//
+//        System.out.println(23 / 3);   //7
+//        System.out.println(23 % 3);             //2
+//        System.out.println();
+//
+//        System.out.println(7/3);      //2
+//        System.out.println(7%3);                //1
+//        System.out.println();
+//
+//        System.out.println(2/3);      //0
+//        System.out.println(2%3);                //2
+//        System.out.println();
+//
+//        int a3 =0212121;
+
+
+
+
+
+
+
+    }
+}
